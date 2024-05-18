2a635329ed67e7fa8dd2d150b15e1ce29927d303
unknown <ahmedgamal56690@gmail.com>
2024-05-18 23:24:12 +0300

    Second Assignment

---
+++ SummationForArrayByFunction.java
@@ -0,0 +1,23 @@
+package Second_02_Assignment;
+static int getSummition(int []arr){
+    int sum = 0;
+    for(int i=0;i<arr.length;i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+public class SummationForArrayByFunction {
+    public static void main(String[]args){
+        int add=0;
+        int []arr = {15,2,3,6,4,8,9,10,20};
+        add = getSummition([]arr);
+
+    }
+
+}
+
+public void main() {
+}
+
+public void main() {
+}
